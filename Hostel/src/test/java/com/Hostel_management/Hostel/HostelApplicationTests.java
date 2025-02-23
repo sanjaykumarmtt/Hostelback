bf32817604084000c13be9fb43e945c89a0319a7
sanjaykumarmtt <147645426+sanjaykumarmtt@users.noreply.github.com>
2025-02-23 12:51:57 +0530

    springback

---
+++ Hostel/src/test/java/com/Hostel_management/Hostel/HostelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Hostel_management.Hostel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
